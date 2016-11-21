f2350630b3b9dd40037ab0ce50834be2df08cc1d
Richard Seldon <arcseldon@gmail.com>
2016-11-21 14:14:26 +0900

    in progress - hack lock

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-$(document).ready(function() {
-  
+$(document).ready(function () {
+
+  // $(".auth0-lock-input").focusout(function () {
+  //   console.log("focus out - fired event!");
+  //   alert("focus out - fired event!");
+  // });
+
+  //auth0-lock-input
+
   var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
     auth: {
       params: { scope: 'openid email' } //Details: https://auth0.com/docs/scopes
     }
   });
 
-  $('.btn-login').click(function(e) {
+  $('.btn-login').click(function (e) {
     e.preventDefault();
     lock.show();
   });
 
-  $('.btn-logout').click(function(e) {
+  $('.btn-logout').click(function (e) {
     e.preventDefault();
     logout();
   })
 
-  lock.on("authenticated", function(authResult) {
-    lock.getProfile(authResult.idToken, function(error, profile) {
+  lock.on("authenticated", function (authResult) {
+    lock.getProfile(authResult.idToken, function (error, profile) {
       if (error) {
         // Handle error
         return;
@@ -29,7 +36,7 @@ $(document).ready(function() {
   });
 
   //retrieve the profile:
-  var retrieve_profile = function() {
+  var retrieve_profile = function () {
     var id_token = localStorage.getItem('id_token');
     if (id_token) {
       lock.getProfile(id_token, function (err, profile) {
@@ -42,14 +49,16 @@ $(document).ready(function() {
     }
   };
 
-  var show_profile_info = function(profile) {
-     $('.nickname').text(profile.nickname);
-     $('.btn-login').hide();
-     $('.avatar').attr('src', profile.picture).show();
-     $('.btn-logout').show();
+  // $(".auth0-lock-input")
+
+  var show_profile_info = function (profile) {
+    $('.nickname').text(profile.nickname);
+    $('.btn-login').hide();
+    $('.avatar').attr('src', profile.picture).show();
+    $('.btn-logout').show();
   };
 
-  var logout = function() {
+  var logout = function () {
     localStorage.removeItem('id_token');
     window.location.href = "/";
   };
